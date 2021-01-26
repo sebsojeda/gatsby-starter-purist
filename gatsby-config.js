d604a633fb9eb414986864907fa67963aa9437ff
Sebastian Ojeda <sebastian.ojeda38@gmail.com>
2021-01-25 23:46:21 -0700

    Bump package version

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,11 @@ module.exports = {
     },
     social: [
       {
-        name: "Twitter",
+        name: "twitter",
         url: "https://twitter.com/sebsojeda",
       },
       {
-        name: "GitHub",
+        name: "gitHub",
         url: "https://github.com/sebsojeda/gatsby-theme-purist",
       },
     ],
